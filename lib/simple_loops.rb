cb11a4bae2e2ae64861b493dbcbd3d3ef4da59f6
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-08 17:53:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,10 +15,10 @@ def loop_message_n_times(message, num)
   end
 end
 
-def output_array(message, num)
+def output_array(array)
   counter = 0 
-  while counter < num do
-    puts message
+  while counter <array.length do
+    puts array[counter]
     counter += 1 
   end
 end
